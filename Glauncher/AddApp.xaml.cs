0d2bc2fa16a69b109a2dd662fcf397273650683d
boss of the gym <super.vladm20@gmail.com>
2022-03-05 17:06:05 +0300

    Добавил окно для добавления других программ.
    Без интерфейса

---
+++ Glauncher/AddApp.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Glauncher
+{
+
+    public partial class AddApp : Window
+    {
+        public static AddApp app;
+
+        private void MovingWindow(object sender, RoutedEventArgs e)   //Метод для перемещения окна на экране
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                AddApp.app.DragMove();
+            }
+        }
+
+        public AddApp()
+        {
+            InitializeComponent();
+            app = this;
+        }
+
+        private void CloseAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            app.Close() ;
+        }
+
+    }
+}
